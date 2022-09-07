1dd46c128ad0180bb943095cb232d60b1478da18
Yorlin Garcia <yorlingarcia96@hotmail.com>
2022-09-07 11:51:01 -0500

    update index.js, funciones enlazadas

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const random = (num) => Math.floor(Math.random() * num);
 
 // Frases de motivacion personal
-const personal_motivation = () => {
+const professional_motivation = () => {
   let num = random(6);
   switch (num) {
     case 0:
@@ -38,180 +38,16 @@ const short_motivation = () => {
 };
 
 // generador de mensajes
-const message = (number) => {
-  let num = random(number);
+const message = () => {
+  let num = random(2);
   switch (num) {
     case 0:
-      return "";
+      console.log(professional_motivation());
+      break;
+    case 1:
+      console.log(short_motivation());
+      break;
   }
 };
 
-/*
-
-Frases motivadoras cortas
-
-
-No busques los errores, busca un remedio - Henry Ford
-
-Tu actitud, no tu aptitud, determinará tu altitud - Zig Ziglar
-Tienes que hacer las cosas que crees que no puedes hacer - Eleanor Roosevelt
-Si te caíste ayer, levántate hoy - H. G. Wells
-
-Si no pierdes, no puedes disfrutar de las victorias - Rafael Nadal
-
-Haz de cada día tu obra maestra John Wooden 
-No cuentes los días, haz que los días cuenten - Muhammad Ali
-
-Si la oportunidad no llama, construye una puerta - Milton Berle
-Deja que cada hombre ejerza el arte que domina - Aristófanes 
-El valor de una idea radica en su uso - Thomas Edison 
-
-
-Frases motivacionales de superación personal
-No estás solo, no eres el único “raro” trabajando en su desarrollo personal, tampoco estás loco por querer hacerlo. Esperamos que estas 15 frases de superación personal te sean de gran ayuda:
-
-
-
-
-Piensa, sueña, cree y atrévete - Walt Disney
-Con autodisciplina casi todo es posible - Theodore Roosevelt
-Los obstáculos son esas cosas atemorizantes que ves cuando apartas los ojos de tu meta - Henry Ford
-Asegúrate de que colocas tus pies en el lugar correcto, y luego mantente firme - Abraham Lincoln
-Las ideas pueden cambiar la vida. A veces a lo único que tienes que abrir la puerta es a una buena idea - Jim Rohn
-La mejor manera para empezar es callándote y empezar a hacer - Walt Disney
-Nadie se desembaraza de un hábito o de un vicio tirándolo de una vez por la ventana; hay que sacarlo por la escalera, peldaño a peldaño - Mark Twain
-Si te caes siete veces, levántate ocho - Proverbio chino
-La perfección no es alcanzable, pero si perseguimos la perfección podemos conseguir la excelencia - Vince Lombardi
-Tienes que esperar cosas de ti mismo antes de poder hacerlas - Michael Jordan
-Transforma tus heridas en sabiduría - Oprah Winfrey
-La edad no es barrera. Es una limitación que pones en tu mente - Jackie Joyner-Kersee
-Solo aquellos que se arriesgan a ir demasiado lejos pueden descubrir lo lejos que pueden llegar - T.S. Eliot
-Del sufrimiento emergieron las almas más fuertes, los caracteres sólidos tienen cicatrices - Khalil Gibran
-El universo no conspira contra ti, pero tampoco se desvía para alinear tu camino - Tim Ferriss
-Frases célebres
-En esta sección encontrarás las mejores frases motivadoras de personas de la historia y la actualidad, que han marcado una diferencia en el mundo con sus acciones y sus frases de éxito. Aquí te dejamos 15 frases célebres:
-
-
-‍
-
-La innovación distingue al líder del seguidor - Steve Jobs 
-Si puedes soñarlo, puedes hacerlo - Walt Disney
-El secreto para salir adelante es comenzar - Mark Twain
-Los obstáculos son esas cosas atemorizantes que ves cuando apartas los ojos de tu meta - Henry Ford
-La vida es como montar en bicicleta. para mantener el equilibrio tienes que avanzar - Albert Einstein 
-El éxito no es definitivo, el fracaso no es fatídico. Lo que cuenta es el valor para continuar - Winston Churchill
-Sé valiente. Toma riesgos. Nada puede sustituir la experiencia - Paulo Coelho
-El futuro pertenece a aquellos que creen en la belleza de sus sueños - Eleanor Roosevelt
-El conocimiento es poder - Francis Bacon
-He aprendido que el valor no es la ausencia de miedo, sino el triunfo sobre él. El hombre valiente no es el que no siente miedo, sino el que lo domina - Nelson Mandela
-Solo puede ser feliz siempre el que sabe ser feliz con todo - Confucio 
-Es la capacidad de resistir a los errores lo que a menudo conduce a un mayor éxito - J. K. Rwolling
-Aferrarse al rencor es como beber veneno y esperar que otra persona muera - Buda
-No pares cuando estés cansado. Para cuando hayas terminado -  Marilyn Monroe‍
-El 80% del éxito se basa simplemente en insistir - Woody Allen
-Frases motivadoras de la vida
-Y cuando no encuentres específicamente qué está fallando, simplemente lee algunas frases de la vida, de seguro te identificas con alguna y das en el clavo. Las 15 frases motivadoras de la vida que necesitas conocer:
-
-
-‍
-
-Cree que puedes y casi lo habrás logrado - Theodore Roosevelt
-No importa lo que te diga la gente, las palabras y las ideas pueden cambiar el mundo - Robin Williams
-Si tienes un sueño y crees en él, corres el riesgo de que se convierta en realidad - Walt Disney
-Nunca se es demasiado viejo para fijar otra meta o para soñar un nuevo sueño - C. S. Lewis
-Lo que no te mata, te hace más fuerte - Friedrich Nietzsche
-El hombre que mueve montañas comienza cargando pequeñas piedras - Confucio
-Buenos amigos, buenos libros y una conciencia tranquila: esa es la vida ideal - Mark Twain
-Un optimista ve una oportunidad en toda calamidad, un pesimista ve una calamidad en toda oportunidad - Winston Churchill
-Solo aquellos que se arriesgan a caer pueden conseguir grandes cosas - Robert F. Kennedy
-El éxito es la suma de pequeños esfuerzos, que se repiten día tras día - Robert Collier
-Sal de tu zona de confort. Solo puedes crecer si estás dispuesto a sentirte raro e incómodo cuando intentas algo nuevo - Brian Tracy
-¿Por qué contentarnos con vivir a rastras cuando sentimos el anhelo de volar? - Hellen Keller
-Entrega siempre más de lo que esperan de ti - Larry Page
-Aprovecha las oportunidades que aparezcan, donde quiera que estén - Lakshmi Mittal 
-Nunca es demasiado tarde para ser lo que podrías haber sido - George Eliot
-Frases positivas de motivación personal
-Estas frases positivas no son solo frases bonitas de desarrollo personal, sino palabras que, leídas con mucha atención, te darán consejos para trabajar en tu crecimiento individual. Ojalá y alguna de estas 15 frases de motivación personal te den la motivación que buscas:
-
-
-‍
-
-Puedo aceptar el fracaso pero no puedo aceptar no intentarlo - Michael Jordan
-La mejor forma de predecir el futuro es creándolo - Abraham Lincoln 
-Un día despertarás y descubrirás que no tienes más tiempo para hacer lo que soñabas. El momento es ahora. Actúa - Paulo Coelho
-Hoy eres un lector y mañana serás un líder - Margaret Fuller
-Los dos guerreros más poderosos son la paciencia y el tiempo - León Tolstói
-No puedes derrotar a la persona que nunca se rinde - Babe Ruth
-El secreto de tu éxito está determinado por tu agenda diaria - John C. Maxwell
-Recordar que vas a morir, es la mejor manera que conozco para no pensar que tienes algo que perder. Ya estás desnudo. No hay ninguna razón para no seguir a tu corazón - Steve Jobs
-Se lo suficientemente valiente para vivir de forma creativa. El lugar creativo donde nadie ha estado - Alan Alda
-Cuando dejo ir lo que soy, me convierto en lo que debería ser - Lao Tzu
-Apunta a la luna. Si fallas, podrías dar a una estrella - William Clement Stone 
-Existe al menos un rincón del universo que con toda seguridad puedes mejorar, y eres tú mismo - Aldous Huxley
-El verdadero buscador crece y aprende, y descubre que siempre es el principal responsable de lo que sucede - Jorge Bucay
-Escribir es un oficio que se aprende escribiendo - Simone de Beauvoir
-La forma más rápida de cambiar es convivir con personas que ya son como quieres ser - Reid Hoffman
-Frases motivadoras sobre cómo ser feliz
-Además de las frases de motivación personal, existen frases de la vida; aquellas que surgen de la experiencia de quien la vive al máximo, y encuentra en ella el camino hacia la felicidad. Te dejamos algunas palabras de motivación para comenzar tu búsqueda:
-
-
-‍
-
-La felicidad no es algo que pospones para el futuro; es algo que diseñas para el presente - Jim Rohn
-La alegría es contagiosa y siempre consigue descubrir una solución donde la lógica sólo encontró una explicación para el error - Paulo Coelho
-Vive la vida que amas. Ama la vida que vives - Bob Marley
-El éxito no es la clave de la felicidad. La felicidad es la clave del éxito - Albert Schweitzer
-Unas palabras amables, un libro cálido y una sonrisa honesta pueden hacer milagros - William Hazlitt
-La verdadera felicidad consiste en hacer el bien - Aristóteles
-El otoño se convierte en una nueva primavera cuando cada hoja es una flor - Albert Camus
-El éxito es encontrar satisfacción en dar un poco más de lo que se recibe  - Christopher Reeve
-La alegría reside en la alegría del logro y en la emoción del esfuerzo creativo - Franklin Roosevelt
-La verdadera motivación procede de trabajar en cosas que nos importan - Sheryl Sandberg
-La felicidad no ocurre por casualidad, sino por elección - Jim Rohn
-No tienes poder sobre los acontecimientos exteriores –pero sí sobre tu mente. Aprende esto, y encontrarás tu fuerza - Marco Aurelio
-La buena vida es una forma de pensar - Emilio Valcárcel
-Puedes ser feliz allí donde estés - Joel Osteen
-El término «felicidad» perdería todo su significado si no fuese compensado por la tristeza - Carl Jung
-Frases inspiradoras
-Copia y pega esta sección en tu bloc de notas para cuando necesites leer una frase motivadora. Entre las frases bonitas, escogimos las mejores 15 frases inspiradoras que te pueden ayudar cuando no tengas ánimos para seguir adelante: 
-
-
-‍
-
-Alguien se sienta en la sombra porque alguien plantó un árbol hace mucho tiempo - Warren Buffett
-Avanza con confianza en la dirección de tus sueños - Thoreau
-La diligencia es la madre de la buena suerte - Benjamin Franklin
-Sólo se vive una vez. Pero si lo haces bien, una vez basta - Mae West
-Da el primer paso con fe. No tienes que ver todas las escaleras, sólo da el  primer paso - Martin Luther King
-Trabajar duro por algo que no te importa se llama estrés. Trabajar duro por algo que te importa de verdad se llama pasión - Simon Sinek
-El éxito depende del esfuerzo - Sófocles
-Intenta y falla, pero nunca falles en intentarlo - Jared Leto 
-No quiero ganarme la vida, quiero vivir - Oscar Wilde
-La vida no es un problema a ser resuelto, sino una realidad a experimentar - Søren Aabye Kierkegaard
-Háblate a ti mismo como harías con alguien a quien amas - Brene Brown
-Por más difícil que parezca la vida, siempre hay algo que puedes hacer y en lo que puedes tener éxito - Stephen Hawking
-El fracaso se convierte en éxito si aprendes de él - Malcolm Forbes
-La mala noticia es que el tiempo vuela. La buena noticia es que tú eres el piloto - Michael Altshuler
-La única manera de encontrar los límites de lo posible es ir más allá de lo imposible - Arthur C. Clarke
-Frases bonitas
-Y si las frases inspiradoras lograron sacarte aunque sea media sonrisa, de seguro estas 15 frases frases bonitas logran completarla. Aquí algunas frases de amor, hacia el resto, y hacia ti mismo:
-
-
-‍
-
-Casi todas las cosas buenas que suceden en el mundo, nacen de una actitud de aprecio por los demás - Dalai Lama 
-Donde reina el amor, siempre sobran las leyes - Platón
-Amarse a uno mismo es el comienzo de una larga vida romántica - Oscar Wilde
-Nada de lo que vistes es más importante que tu sonrisa - Connie Stevens
-En cada forma de vida existe el deseo del amor - D. H. Lawrence
-Todos nos necesitamos los unos a los otros - Leo Buscaglia
-Antes moriría de pasión que de aburrimiento - Vincent van Gogh
-Solo hay una felicidad en la vida, amar y ser amado - George Sand
-El amor propio es la fuente de todos los tipos de amor - Pierre Corneille
-Cambias tu vida al cambiar tu corazón - Max Lucado
-La sencillez es una forma de humildad y la sencillez es una señal de la verdadera grandeza - Vince Lombardi
-El alma que hablar puede con los ojos, también puede besar con la mirada - Gustavo Adolfo Bécquer 
-El que ama, se hace humilde. Aquellos que aman, por decirlo de alguna manera, renuncian a una parte de su narcisismo - Sigmund Freud
-Sólo porque alguien no te ame como tú quieres, no significa que no te ame con todo su ser - Gabriel García Márquez
-El amor no mira con los ojos, sino con el alma - William Shakespeare
-*/
+message();
